6ee106eed8eb316b8464da5aaeeca314482a89a6
1162474984@qq.com <BAOguan789>
2020-04-08 22:45:21 +0800

    zuul待解决

--- cloud_blog/blog_zuul_6001/src/main/java/com/jxy/blog/ZuulApplication.java
+++ cloud_blog/blog_zuul_6001/src/main/java/com/jxy/blog/ZuulApplication.java
@@ -1,13 +1,19 @@
 package com.jxy.blog;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 /**
  * @author jxy
  * @className ZuulApplication
  * @description 描述
  * @date 2020/4/7 20:20
  */
+@SpringBootApplication
+@EnableZuulProxy
 public class ZuulApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(ZuulApplication.class, args);
     }
 }
